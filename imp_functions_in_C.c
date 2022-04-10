802797fcfaef4d699ef5175a1db23cb485cc085f
Navansh <21141664+Navansh@users.noreply.github.com>
2022-04-10 15:11:48 +0530

    Create imp_functions_in_C.c

---
+++ imp_functions_in_C.c
@@ -0,0 +1,10 @@
+ atan2- The C library function double atan2(double y, double x) returns the arc tangent in radians of y/x based on the signs of both values to determine the correct quadrant.
+
+
+
+
+
+pow(2,4) ;   used to write 2 to the power of 4 
+
+remember to use #include <math.h> 
+
